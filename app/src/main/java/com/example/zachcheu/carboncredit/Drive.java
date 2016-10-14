fb288408a620f9db0ba3dcba120d4daacf56829e
Zechariah Cheung <ZachCheu@Zechariahs-MacBook-Pro.local>
2016-10-14 09:34:48 -0700

    DriveComponents

--- app/src/main/java/com/example/zachcheu/carboncredit/Drive.java
+++ app/src/main/java/com/example/zachcheu/carboncredit/Drive.java
@@ -1,33 +1,30 @@
 package com.example.zachcheu.carboncredit;
-
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.widget.TextView;
 
-import com.mapbox.mapboxsdk.maps.MapView;
-
-/**
- * Created by ZachCheu on 10/13/16.
- */
-public class Drive extends ActionBarActivity implements LocationListener {
+public class Drive extends Activity implements LocationListener {
 
     // gets context location!
     private LocationManager lm;
     private TextView textView;
-    public MapView mapView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drive);
 
-        textView = (TextView) findViewById(R.id.speedView);
+        textView = (TextView) findViewById(R.id.speed);
         lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 1, this);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "gothic.ttf");
+        textView.setTypeface(typeface);
+
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
     }
 
     @Override
@@ -35,7 +32,8 @@ public class Drive extends ActionBarActivity implements LocationListener {
         Drive.this.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                textView.setText(String.valueOf((int) (location.getSpeed()*2.2369)));
+                Log.d("Test"," "+location.getSpeed());
+                textView.setText(String.valueOf((location.getSpeed()*2.2369)));
             }
         });
     }
@@ -54,35 +52,4 @@ public class Drive extends ActionBarActivity implements LocationListener {
     public void onProviderDisabled(String provider) {
         //TO DO!
     }
-
-    @Override
-    public void onPause(){
-        super.onPause();
-        //mapView.onPause();
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        //mapView.onResume();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        //mapView.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory(){
-        super.onLowMemory();
-        //mapView.onLowMemory();
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        //mapView.onDestroy();
-    }
 }
-
