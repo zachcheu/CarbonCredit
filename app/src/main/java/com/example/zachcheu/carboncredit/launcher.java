2315b62e10c01c5d81e788e1cde639ffac77fcf5
DevWork <s-RRashid@lwsd.org>
2016-10-27 09:02:39 -0700

    third actual commit

--- app/src/main/java/com/example/zachcheu/carboncredit/launcher.java
+++ app/src/main/java/com/example/zachcheu/carboncredit/launcher.java
@@ -1,8 +1,17 @@
 package com.example.zachcheu.carboncredit;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by DevWork on 10/27/16.
  */
 
-public class launcher {
+public class launcher extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView();
+    }
 }
